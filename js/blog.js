9e3b3e34afdd8033ac7224aec677a25e8c7ce37c
NikolaJelesijevic77 <19-2021@ftn.edu.rs>
2024-07-08 15:33:59 +0200

    blog page

--- js/blog.js
+++ js/blog.js
@@ -1,40 +1,42 @@
-const fetchData = async (url) =>
-    {
-        try 
-        {
-            const response = await fetch(url);
-            if(!response.ok)
-            {
-                throw new Error('Error fetching data ' + response.status);
-            }
-            const data = await response.json();
-            console.log(data);
-            let tmpString="";
-            var myDataContainer = document.getElementById('blogCards');
-            let x = data?.blogovi.map((blog) => `
-            <div class="col col-12 col-md-6 col-lg-4 mb-3">
-                <div class="card h-100">
-                    <div class="card-header">
-                        Naslov:${blog.naslov}
-                        Autor: ${blog.autor}
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">Datum:${blog.datum} </p>
-                        <h5 class="card-title">Id: ${blog.id}</h5>
-                        <p class="card-text"> Opis: ${blog.sadrzaj}</p>
-                    </div>
-                    <div class="card-footer">
-                        <p class="card-text"> Slug: ${blog.slug}</p>
-                    </div>
-                </div>
-            </div>`)
-            tmpString += x;
-            myDataContainer.innerHTML = tmpString;
-        } 
-        catch (error) 
-        {
-            console.error(error.message);
+const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Error fetching data ' + response.status);
         }
+        const data = await response.json();
+        console.log(data);
         
+        let tmpString = "";
+        var myDataContainer = document.getElementById('blogCards');
+        
+        data?.blogovi.forEach((blog) => {
+            tmpString += `
+                <div class="col mb-4">
+                    <div class="card card-custom h-100">
+                        <div class="card-header card-header-custom">
+                            Id: ${blog.id}
+                        </div>
+                        <div class="card-body card-body-custom">
+                            <h4 class="card-title card-title-custom"><b>Naslov:</b> ${blog.naslov}</h4>
+                            <p class="card-text card-text-custom"><b>Opis:</b> ${blog.sadrzaj}</p>
+                            <p class="card-text card-text-custom"><b>Autor:</b> ${blog.autor}</p>
+                            <h6 class="card-title"><b>Datum:</b> ${blog.datum}</h6>
+                            <div class="d-flex justify-content-center">
+                                <button class="btn btn-custom mt-3">Ceo blog!</button>
+                            </div>
+                        </div>
+                        <div class="card-footer card-footer-custom">
+                            <small>${blog.slug}</small>
+                        </div>
+                    </div>
+                </div>`;
+        });
+
+        myDataContainer.innerHTML = tmpString;
+        
+    } catch (error) {
+        console.error(error.message);
     }
-fetchData("../db/blog.json");
+}
+fetchData("../db/blog.json")
